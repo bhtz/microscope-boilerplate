40b7f4ca34a10ad77e06f88bde46478cde74cb8e
Benjamin HEINTZ <heintz.benjamin@gmail.com>
2024-04-10 00:47:54 +0200

    documentation:build

---
+++ docs/assets/templates_cli.md.DzgtUjkG.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a5 as o}from"./chunks/framework.B1C_J5Tt.js";const u=JSON.parse('{"title":"CLI","description":"","frontmatter":{},"headers":[],"relativePath":"templates/cli.md","filePath":"templates/cli.md"}'),l={name:"templates/cli.md"},s=o("",7),i=[s];function c(n,p,r,d,h,m){return t(),a("div",null,i)}const C=e(l,[["render",c]]);export{u as __pageData,C as default};
