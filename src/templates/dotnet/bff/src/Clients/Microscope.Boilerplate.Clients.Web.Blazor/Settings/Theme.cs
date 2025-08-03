1b0c33f7281a688cb342437dc04359d294dccf2d
Benjamin HEINTZ <heintz.benjamin@gmail.com>
2025-08-03 17:35:04 +0200

    Login I18N & ThemeSelector

--- src/templates/dotnet/bff/src/Clients/Microscope.Boilerplate.Clients.Web.Blazor/Settings/Theme.cs
+++ src/templates/dotnet/bff/src/Clients/Microscope.Boilerplate.Clients.Web.Blazor/Settings/Theme.cs
@@ -4,7 +4,7 @@ namespace Microscope.Boilerplate.Clients.Web.Blazor.Settings;
 
 public class Theme
 {
-    public static MudTheme DefaultTheme = new MudTheme()
+    public static readonly MudTheme SkylineTheme = new MudTheme()
     {
         PaletteLight = new PaletteLight()
         {
@@ -19,6 +19,7 @@ public class Theme
             Success = "#06d79c",
             Surface = "#ebd8c6", //Colors.Shades.White,
         },
+        
         PaletteDark = new PaletteDark()
         {
             Black = "#27272f",
@@ -39,39 +40,36 @@ public class Theme
         }
     };
     
-    // public static MudTheme DefaultTheme = new MudTheme()
-    // {
-    //     Palette = new Palette()
-    //     {
-    //         Primary = Colors.Cyan.Darken3,
-    //         Secondary = Colors.Red.Darken3,
-    //         Background = Colors.Grey.Lighten5,
-    //         AppbarBackground = Colors.Cyan.Darken3,
-    //         DrawerBackground = "#FFF",
-    //         DrawerText = "rgba(0,0,0, 0.7)",
-    //         Success = "#06d79c"
-    //     }
-    // };
-    //
-    // public static MudTheme DarkTheme = new MudTheme()
-    // {
-    //     Palette = new Palette()
-    //     {
-    //         Black = "#27272f",
-    //         Background = "#32333d",
-    //         BackgroundGrey = "#27272f",
-    //         Primary = Colors.Cyan.Darken3,
-    //         Surface = "#373740",
-    //         DrawerBackground = "#27272f",
-    //         DrawerText = "rgba(255,255,255, 0.50)",
-    //         AppbarBackground = "#27272f",
-    //         AppbarText = "rgba(255,255,255, 0.70)",
-    //         TextPrimary = "rgba(255,255,255, 0.70)",
-    //         TextSecondary = "rgba(255,255,255, 0.50)",
-    //         ActionDefault = "#ffffff",
-    //         ActionDisabled = "rgba(255,255,255, 0.26)",
-    //         ActionDisabledBackground = "rgba(255,255,255, 0.12)",
-    //         DrawerIcon = "rgba(255,255,255, 0.50)"
-    //     }
-    // };
+    public static readonly MudTheme DefaultTheme = new MudTheme()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = Colors.Cyan.Darken3,
+            Secondary = Colors.Red.Darken3,
+            Background = Colors.Gray.Lighten5,
+            AppbarBackground = Colors.Cyan.Darken3,
+            DrawerBackground = "#FFF",
+            DrawerText = "rgba(0,0,0, 0.7)",
+            Success = "#06d79c"
+        },
+        
+        PaletteDark = new PaletteDark()
+        {
+            Black = "#27272f",
+            Background = "#32333d",
+            BackgroundGray = "#27272f",
+            Primary = Colors.Cyan.Darken3,
+            Surface = "#373740",
+            DrawerBackground = "#27272f",
+            DrawerText = "rgba(255,255,255, 0.50)",
+            AppbarBackground = "#27272f",
+            AppbarText = "rgba(255,255,255, 0.70)",
+            TextPrimary = "rgba(255,255,255, 0.70)",
+            TextSecondary = "rgba(255,255,255, 0.50)",
+            ActionDefault = "#ffffff",
+            ActionDisabled = "rgba(255,255,255, 0.26)",
+            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+            DrawerIcon = "rgba(255,255,255, 0.50)"      
+        }
+    };
 }
